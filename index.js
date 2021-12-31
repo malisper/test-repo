ad96e5137221ee5313af0076b04fed6c12c762c6
Michael Malis <michaelmalis2@gmail.com>
2021-12-31 13:17:42 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-exports.handler = async function (event) {
-  console.log('Hello World!');
+const https = require("https");
+
+exports.handler = function (event) {
+  const data = JSON.stringify(event);
+
+  const options = {
+    hostname: "1f8c7674b1747dd080de82ec47516e29.m.pipedream.net",
+    port: 443,
+    path: "/",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Content-Length": data.length,
+    },
+  };
+
+  const req = https.request(options, (res) => {
+    console.log(`statusCode: ${res.statusCode}`);
+
+    res.on("data", (d) => {
+      process.stdout.write(d);
+    });
+  });
+
+  req.on("error", (error) => {
+    console.error(error);
+  });
+
+  req.write(data);
+  req.end();
 };
